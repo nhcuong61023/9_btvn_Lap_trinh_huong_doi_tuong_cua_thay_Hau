4cef9ff31062d4514fdca1c4a0b92ef973a1d893
nhcuong61023 <97533348+nhcuong61023@users.noreply.github.com>
2022-05-30 21:07:04 +0700

    Update btvn_b7_Mang_cap_phat_dong_1_chieu.cpp

--- btvn_b7_Mang_cap_phat_dong_1_chieu.cpp
+++ btvn_b7_Mang_cap_phat_dong_1_chieu.cpp
@@ -6,95 +6,80 @@ using namespace std;
 
 int random(int minN, int maxN)
 {
-    return minN + rand() % (maxN + 1 - minN);
+   return minN + rand() % (maxN + 1 - minN);
 }
 
 int main()
 {
-    srand((int)time(0));
-    int a;
-    int m, n;
-    cout << "Nhap doan m, n: ";
-    cin >> m >> n;
+   srand((int)time(0));
+   int a;
+   cout << "So Phan Tu Cua Mang la: ";
+   cin >> a;
+   int m, n;
+   cout << "Nhap doan m, n: ";
+   cin >> m >> n;
+   cout << "(Cac gia tri cua mang se duoc lay ngay nhien tu doan (m,n))" << endl;
+   float *ptr;
+   // Cấp phát `a` ô nhớ kiểu float
+   ptr = new float[a];
+   for (int i = 0; i < a; i++) // Nhập mảng động 1 chiều
+   {
+      ptr[i] = random(m, n);
+   }
+   for (int i = 0; i < a; ++i)
+   {
+      cout << "Phan tu thu " << i << " :" << *(ptr + i) << endl; // In ra mảng động 1 chiều
+   }
 
-    for (int i = 0; i < n; ++i)
-    {
-        a = random(m, n);
-    }
-    cout << "So Phan Tu Cua Mang la: ";
-    cout << a;
-    cout << "\nHay Nhap so luong phan tu mang: " << endl;
+   // Tìm số dương nhỏ nhất
+   int min = 0;
+   for (int i = 0; i < a; i++)
+   {
 
-    float *ptr;
-    // Cấp phát `a` ô nhớ kiểu float
-    ptr = new float[a];
-    for (int i = 0; i < a; i++)// Nhập mảng động 1 chiều
-    {
-        cout << "a[" << i << "]: ";
-        cin >> ptr[i];
-    }
-    for (int i = 0; i < a; ++i)
-    {
-        cout << "Phan tu thu " << i << " :" << *(ptr + i) << endl; // In ra mảng động 1 chiều
-    }
+      if (ptr[i] > 0)
+      {
+         min = ptr[i];
+         break;
+      }
+   }
+   for (int i = 0; i < a; i++)
+   {
+      if (ptr[i] > 0 && ptr[i] < min)
+         min = ptr[i];
+   }
+   if (min == 0)
+   {
+      cout << "\nkhong co so duong nho nhat";
+   }
+   else
+   {
+      cout << "\nSo duong Nho Nhat la: " << min;
+   }
+   // Tìm số âm lớn nhất
+   int max = 0;
+   for (int i = 0; i < a; i++)
+   {
 
-    // Tìm số dương nhỏ nhất
-    int min = 0;
+      if (ptr[i] < 0)
+      {
+         max = ptr[i];
+         break;
+      }
+   }
+   for (int i = 0; i < a; i++)
+   {
+      if (ptr[i] < 0 && ptr[i] > max)
+         max = ptr[i];
+   }
+   if (max == 0)
+   {
+      cout << "\nkhong co so am lon nhat";
+   }
+   else
+   {
+      cout << "\nSo Am lon Nhat la: " << max;
+   }
 
-    for (int i = 0; i < n; i++)
-    {
-
-        if (ptr[i] > 0)
-        {
-
-            min = ptr[i];
-            break;
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-
-        if (ptr[i] > 0 && ptr[i] < min)
-            min = ptr[i];
-    }
-    if (min == 0)
-    {
-        cout << "khong co so duong nho nhat";
-    }
-    else
-    {
-        cout << "\nSo duong Nho Nhat la: " << min;
-    }
-    // Tìm số âm lớn nhất
-    int max;
-    int demsoam = 0;
-    for (int i = 0; i < n; i++)
-    {
-
-        if (ptr[i] < 0)
-        {
-
-            max = ptr[i];
-            break;
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-
-        if (ptr[i] < 0 && ptr[i] > max)
-            max = ptr[i];
-    }
-    
-    if (max == 0)
-    {
-        cout << "khong co so am lon nhat";
-    }
-    else
-    {
-        cout << "\nSo Am lon Nhat la: " << max;
-    }
-
-    delete[] ptr; // Giải Phóng bộ nhớ
-
-    return 0;
+   delete[] ptr; // Giải Phóng bộ nhớ
+   return 0;
 }
